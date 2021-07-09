6ecfcb7a866f92fbf86a7e49586fb05ae2e7d178
unknown <APRENDIZ@SGLAPRCATFSP314.FormacionSena.RED>
2021-07-09 08:09:57 -0500

    img home

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,7 +1,10 @@
 <template>
     
     <div>
-        <!--<v-img  src="../assets/logo.png" ></v-img>-->
+        <!--<v-img  src="../assets/logo.png" styele="max-width:50px;max-height:50px;"></v-img>-->
+          <img srcset="../assets/logo.png 320w,
+             ../assets/logo.png 480w,
+             ../assets/logo.png 800w"  sizes="(max-width: 320px) 280px,   (max-width: 480px) 440px,   800px"  src="../assets/logo.png" >
     </div>
     
 </template>
