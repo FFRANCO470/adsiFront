f089c3c733e4df4444d45942f7abc298a16fde18
unknown <APRENDIZ@SGLAPRCATFSP314.FormacionSena.RED>
2021-08-10 14:53:00 -0500

    w

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,9 +2,7 @@
     
     <div>
         <!--<v-img  src="../assets/logo.png" styele="max-width:50px;max-height:50px;"></v-img>-->
-          <img srcset="../assets/logo.png 320w,
-             ../assets/logo.png 480w,
-             ../assets/logo.png 800w"  sizes="(max-width: 320px) 280px,   (max-width: 480px) 440px,   800px"  src="../assets/logo.png" >
+        <img src="../assets/logo.png">
     </div>
     
 </template>
